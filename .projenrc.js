51df6ab46c75dff91dd6aad368906fcea7a89438
Matthew Bonig <matthew.bonig@gmail.com>
2021-09-01 10:50:08 -0600

    actually writing to DDB

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,13 @@ const project = new AwsCdkTypeScriptApp({
   cdkVersion: '2.0.0-rc.18',
   defaultReleaseBranch: 'main',
   name: 'custom-resources',
-  deps: ['@types/aws-lambda']
+  deps: [
+    '@types/aws-lambda',
+    '@aws-sdk/client-dynamodb',
+    '@aws-sdk/lib-dynamodb',
+    '@aws-sdk/util-dynamodb',
+    'esbuild',
+  ],
   // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
--- src/fixture-handler.ts
+++ src/fixture-handler.ts
@@ -1,12 +1,28 @@
-import { CloudFormationCustomResourceEvent } from "aws-lambda";
-import { send, SUCCESS } from './cfn-response';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { CloudFormationCustomResourceEvent } from 'aws-lambda';
+import { FAILED, send, SUCCESS } from './cfn-response';
 
-const tableName = process.env.TABLE_NAME;
 
 export const handler = async (event: CloudFormationCustomResourceEvent, context: any) => {
+  const tableName = process.env.TABLE_NAME;
   console.log('Event: ', JSON.stringify(event, null, 2));
   console.log('TableName: ', tableName);
+  const records = event.ResourceProperties.records;
+  console.log('Records:', records);
 
-  // start making some DynamoDB calls.
-  await send({ ...event, PhysicalResourceId: context.logStreamName }, SUCCESS, {});
-}
+  if (event.RequestType === 'Delete') {
+    return send({ ...event, PhysicalResourceId: context.logStreamName }, SUCCESS, {});
+  }
+
+  const client = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+  try {
+    for (const record of records) {
+      await client.send(new PutItemCommand({ Item: record, TableName: tableName }));
+    }
+    await send({ ...event, PhysicalResourceId: context.logStreamName }, SUCCESS, {});
+  } catch (err) {
+    console.error('Error occurred while trying to write records', err);
+    await send({ ...event, PhysicalResourceId: context.logStreamName }, FAILED, err);
+  }
+};
