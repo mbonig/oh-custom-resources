1c1fb9128f5f35d5362868d58cc59ab49f093d28
Matthew Bonig <matthew.bonig@gmail.com>
2021-09-28 07:39:53 -0600

    removing build workflow

--- .projenrc.js
+++ .projenrc.js
@@ -10,12 +10,13 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-sdk/util-dynamodb',
     'esbuild',
   ],
+  releaseWorkflow: false,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
+  buildWorkflow: false,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
   // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
   // packageName: undefined,            /* The "name" in package.json. */
   // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
 });
 project.synth();
